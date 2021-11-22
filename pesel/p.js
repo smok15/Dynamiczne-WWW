259638bb30a165ef57cfa96105358fce84d74564
smok15 <224800@edu.p.lodz.pl>
2021-11-22 18:48:15 +0100

    FWW10

--- pesel/p.js
+++ pesel/p.js
@@ -3,7 +3,9 @@ function validate(){
   var worker = new Worker('validate_one_pesel.js');
 
   worker.addEventListener('message', function(e) {
-    alert('otrzymano odpowiedź: ' + e.data);
+    var myDiv = document.getElementById("response");
+    myDiv.innerHTML = e.data;
+    //alert('otrzymano odpowiedź: ' + e.data);
   }, false);
 
   var year = document.getElementById("wpisz_rok").value;
