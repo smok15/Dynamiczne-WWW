73bda82dee3fd0bfcd162857e62921b6ab374218
Sebastian Dresler <224800@edu.p.lodz.pl>
2021-11-23 14:01:20 +0100

    FWW11

--- pesel/find_valid_pesels.js
+++ pesel/find_valid_pesels.js
@@ -1,11 +1,39 @@
 
 this.addEventListener('message', function(e) {
     var data = e.data;
-    this.postMessage(dateGenerator());
+    this.postMessage(dateGenerator(data));
   }, false);
 
-  function dateGenerator(){
-      var date = new Date(1930,0,1);
-      date.setTime(date.getTime() + 86400000);
-      return date;
+  function dateGenerator(data){
+      var startDate = new Date(1930,0,1);
+      var endDate = new Date(2050,0,1);
+      var year; var day; var month; var pesel = "";
+      //while(startDate.getTime() !== endDate.getTime()){
+        day = startDate.getDate();
+        month = startDate.getMonth()+1;
+        year = startDate.getFullYear();
+        pesel = year.toString().substr(2,3) + fillZero(month.toString()) + fillZero(day.toString()) + data;
+      //}
+      //date.setTime(date.getTime() + 86400000);
+      return pesel;
+  }
+
+  function fillZero(num){
+    var blank = "00";
+    var result ="";
+    return result = blank.substr(0,2-num.length)+num;
+    }
+
+  function validate(pesel){
+    let weight = [1,3,7,9,1,3,7,9,1,3];
+    let sum =0;
+    let controlNum = parseInt(pesel.substring(10, 11));
+  
+    for(let i = 0; i<weight.length;i++){
+      sum += (parseInt(pesel.substring(i, i + 1)) * weight[i]);
+    }
+    sum = sum % 10;
+    if((10 - sum) % 10 === controlNum){
+      return true;
+    }else return false;
   }
