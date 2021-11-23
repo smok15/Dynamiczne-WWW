b8cb62ee6827ff7cacf832e477e9c4acfcffd719
Sebastian Dresler <224800@edu.p.lodz.pl>
2021-11-23 11:57:08 +0100

    FWW11

--- pesel/find_valid_pesels.js
+++ pesel/find_valid_pesels.js
@@ -5,6 +5,6 @@ this.addEventListener('message', function(e) {
   }, false);
 
   function dateGenerator(){
-      var date = new Date(1930,1,1);
+      var date = new Date(1930,0,1);
       return date;
   }
